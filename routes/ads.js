27f075279479342f35aff2dbe6125ba0555ea69c
msporich <marksporich@gmail.com>
2020-12-06 23:26:08 -0500

    added edit functionality, plus basic styling

--- routes/ads.js
+++ routes/ads.js
@@ -43,10 +43,46 @@ router.post('/create', function (req, res, next) {
     )
 })
 
-/* GET Ad Deletion Page */
+/* GET Ad Deletion Functionality */
 router.get('/delete/:_id', function (req, res, next) {
     var _id = req.params._id;
     Ad.remove({ _id: _id }, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            res.redirect('/ads')
+        }
+    })
+})
+
+/* GET Ad Editing Functionality */
+router.get('/edit/:_id', function (req, res, next) {
+    var _id = req.params._id;
+    Ad.findById(_id, (err, ads) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            res.render('ads/edit', {
+                ads: ads,
+            });
+        }
+    })
+})
+
+/* Ad Editing Submission */
+router.post('/edit/:_id', (req, res, next) => {
+    var _id = req.params._id;
+    var ad = new Ad({
+        _id: _id,
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price
+    })
+    Ad.update({ _id: _id }, ad, (err) => {
         if (err) {
             console.log(err);
             res.end(err);
